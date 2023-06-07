9cb2546803adbdacb5fb56e222bf7b55cda1480a
Derrick Bradbury <derrickb@halex.com>
2023-06-07 13:12:09 -0400

    updates to verify

--- script/deployDiamondFactory.s.sol
+++ script/deployDiamondFactory.s.sol
@@ -24,14 +24,15 @@ contract deployDiamondFactoryScript is Script {
 
     function verifyContract(string memory _facetName, address _addr) internal  {
         console.log("Verify:",_facetName,_addr);
-        string [] memory cmd = new string[](7);
+        string [] memory cmd = new string[](8);
         cmd[0] = "forge";
         cmd[1] = "verify-contract";
         cmd[2] = Strings.toHexString(uint160(_addr), 20);        
         cmd[3] = _facetName;
-        cmd[4] = "6C9YAI39WV3XAMFE5QFYW6RJI25T138RHW";
-        cmd[5] = "--verifier-url";
-        cmd[6] = "https://api.bscscan.com/api";
+        cmd[4] = "-e";
+        cmd[5] = vm.envString("BSCSCANAPIKEY");
+        cmd[6] = "--chain 56";
+        cmd[7] = "--constructor-args 000000000000000000000000500fad360bc10ec706974b999b5f0d615c59feb70000000000000000000000000e0435b1ab9b9dcddff2119623e25be63ef5cb6e00000000000000000000000042a515c1edb651f4c69c56e05578d2805d6451eb";
 
         for(uint i=0;i<cmd.length;i++)
             console.log(i,cmd[i]);
@@ -41,4 +42,3 @@ contract deployDiamondFactoryScript is Script {
     }
 
 }
-
